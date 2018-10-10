6a2202c56bb1e0a88fffa7f163d2f43ddcb4b333
jgomez <jesus.gomez@trivago.com>
2018-10-10 17:13:11 +0200

    Add HTTPS async transport

---
+++ lib/Elastica/Transport/HttpsAsync.php
@@ -0,0 +1,17 @@
+<?php
+namespace Elastica\Transport;
+
+/**
+ * Elastica HttpAsync Transport object.
+ *
+ * @author Jesus Gomez
+ */
+class HttpsAsync extends HttpAsync
+{
+    /**
+     * Https scheme.
+     *
+     * @var string Http scheme
+     */
+    protected $_scheme = 'https';
+}
